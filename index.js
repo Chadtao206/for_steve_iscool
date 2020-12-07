f21d9eb1e1f3c6de680d72eab7e35f6cffbbc4d7
Chadtao206 <chenchen408@gmail.com>
2020-12-06 17:26:56 -0800

    ok

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/sayhi/:myname", (req, res) => res.json(`HELLO ${req.params.myname}`));
-app.post("/login", ({username, password}, res) => {
+app.post("/login", ({ username, password }, res) => {
     if (username === "steve" && password === "iscool") {
-        res.json("HELLO WELCOME!")
+        res.json("HELLO WELCOME!").end();
     }
     res.json("NEW PHONE WHO DIS")
-})
+});
+
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log("HI STEVE!"));
