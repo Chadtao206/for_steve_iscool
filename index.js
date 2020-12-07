f022dd3a6332680c41bc1a732177ccf9ed10e609
Chadtao206 <chenchen408@gmail.com>
2020-12-06 17:17:46 -0800

    ok

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.get("/sayhi/:myname", (req, res) => res.json(`HELLO ${req.params.myname}`));
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=> console.log("HI STEVE!"))
+app.listen(PORT, () => console.log("HI STEVE!"));
