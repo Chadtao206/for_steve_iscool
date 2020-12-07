32b6843fdcae0071ea3a7ad954c6417f8332e8e9
Chadtao206 <chenchen408@gmail.com>
2020-12-06 17:14:48 -0800

    oops

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const path = require("path");
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log("HI STEVE!"))
